002f37c60130f43fe240fbde6633bf800967daff
inferno <inferno3086@ukr.net>
2024-07-01 13:43:42 +0300

    Added more logging and checks to ensure dynamic content is loaded

--- app.py
+++ app.py
@@ -15,13 +15,14 @@ def index():
     return "Welcome to Svitloe Monitor API. Use /widget to get the status."
 
 def get_light_status():
+    status_text = ""
     try:
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-application-cache")
-        options.add_argument("--incognito")  # Use incognito mode to prevent caching
+        options.add_argument("--incognito")
         options.add_argument("--disable-cache")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-gpu")
